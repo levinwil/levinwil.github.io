09f6cee6d27c3a5e83faaf55129501beaade945d
Will <levinwil@outlook.com>
2018-10-02 09:58:07 -0500

    new APL

--- pages/APL.js
+++ pages/APL.js
@@ -4,7 +4,7 @@ import Layout from '../components/Layout'
 var bic = <a style={{'color': '#caa669'}} href='https://github.com/levinwil/User-Friendly-Image-Classifier'>user-friendly binary image classifier</a>
 
 var aplWork = [
-  <span>improving a spoof image detector F1 score from 95% to 99%. I did so by using a custom VGG-based feature extractor network, image augmentation, 10-fold cross-validation, and transfer learning with an SVM</span>,
+  <span>improving a spoof image detector F1 score from 95% to 99%. I did so by implementing a Siamese network with custom image augmentation, 10-fold cross-validation, and transfer learning with an SVM</span>,
   <span>designing a {bic} </span>,
   <span>parallelizing and distributing a previously sequential network detetion algorithm on graphs of more than 2 million vertices. I achieved linear speedup with respect to number of processors using Scala and Spark</span>,
   <span>presenting the results of my code to a group of more than 30 executives, leading to the incorporation of it into the government code-based</span>
